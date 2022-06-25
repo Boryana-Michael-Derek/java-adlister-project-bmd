6b574aad9ff4aafb19af9d218a29ca90b8a529b8
Boryana1 <boryana.dimova1@gmail.com>
2022-06-25 15:43:58 -0500

    Completed edit profile functionality

--- src/main/java/com/codeup/adlister/controllers/EditProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditProfileServlet.java
@@ -1,5 +1,6 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
@@ -23,6 +24,22 @@ public class EditProfileServlet extends HttpServlet {
 
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        System.out.println(username + " " + password + " " + email);
 
+        user.setUsername(username);
+        user.setEmail(email);
+        if(!password.equals("")) {
+            user.setPassword(password);
+        }
+        DaoFactory.getUsersDao().update(user);
+        response.sendRedirect("/profile");
     }
 }
--- src/main/webapp/WEB-INF/profile/edit.jsp
+++ src/main/webapp/WEB-INF/profile/edit.jsp
@@ -6,6 +6,7 @@
     </jsp:include>
 </head>
 <body>
+<jsp:include page="/WEB-INF/partials/navbar.jsp" />
 <div class="container">
     <h1>Edit your profile</h1>
     <form action="/profile/edit" method="POST">
@@ -14,12 +15,12 @@
             <input id="username" name="username" class="form-control" type="text" value="${username}">
         </div>
         <div class="form-group">
-            <label for="password">Password:</label>
-            <input id="password" name="password" class="form-control" type="password">
-        </div>
-        <div class="form-group">
             <label for="email">Email:</label>
             <input id="email" name="email" class="form-control" type="text" value="${email}">
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input id="password" name="password" class="form-control" type="password">
         </div>
         <input type="submit" class="btn btn-block btn-primary">
     </form>
