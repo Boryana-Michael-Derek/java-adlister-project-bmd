fd27e4daa39571281b9b25a8bb9ba47ffef5665d
Boryana1 <boryana.dimova1@gmail.com>
2022-06-27 16:09:24 -0500

    The errors when the username, email or password do not match the criteria are done.

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 import static java.lang.System.out;
 
@@ -24,85 +25,33 @@ public class RegisterServlet extends HttpServlet {
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
 
-        // validate input
-//        boolean inputHasErrors = username.isEmpty()
-//            || email.isEmpty()
-//            || password.isEmpty()
-//            || (! password.equals(passwordConfirmation));
-        boolean usernameNotExist = false;
-        boolean emailNotExist = false;
-        boolean userConflicts = false;
 
-        User nameTest = DaoFactory.getUsersDao().findByUsername(username);
+        User userTest = DaoFactory.getUsersDao().findByUsername(username);
         User emailTest = DaoFactory.getUsersDao().findByEmail(email);
 
-        if(nameTest == null){
-            usernameNotExist = true;
-        }
-        if(emailTest == null){
-            emailNotExist = true;
-        }
-        if(usernameNotExist && emailNotExist){
-            userConflicts = true;
-        }
-
-        String usernameExistMessage = "<script>alert('The Username already exists, try again');</script>";
-        String emailExistMessage = "<script>alert('The email already exist with another user, try again');</script>";
-        String emaiNUsernameExistMessage = "<script>alert('The username and email already exist with another user, try again');</script>";
-
-        if (userConflicts) {
-            User user = new User(username, email, password);
-            DaoFactory.getUsersDao().insert(user);
-            response.sendRedirect("/login");
-        } else if (!usernameNotExist && !emailNotExist) {
-// <<<<<<< hannah-paterka
-            request.setAttribute("passwordError", "The password is incorrect!");
-
-            out.println("<script>");
-            out.println("alert('This username and email is already in use, please try different ones.');");
-            out.println("window.location.replace('" + "/register" + "');");
-            out.println("</script>");
-//             request.setAttribute("usernameExistMessage", usernameExistMessage);
-//             request.setAttribute("emailExistMessage", emailExistMessage);
-// =======
-            request.setAttribute("emaiNUsernameExistMessage", emaiNUsernameExistMessage);
-//             request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
-// >>>>>>> master
-        } else if (!emailNotExist) {
-            out.println("<script>");
-            out.println("alert('This email is already in use, please try another.');");
-            out.println("window.location.replace('" + "/register" + "');");
-            out.println("</script>");
-            request.setAttribute("username", username);
-            request.setAttribute("emailExistMessage", emailExistMessage);
-        } else {
-            out.println("<script>");
-            out.println("alert('This username is already in use, please try another.');");
-            out.println("window.location.replace('" + "/register" + "');");
-            out.println("</script>");
-            request.setAttribute("email", email);
-            request.setAttribute("usernameExistMessage", usernameExistMessage);
-        }
-
+        boolean correctInfo = false;
         if(username.isEmpty()){
             request.setAttribute("usernameError", "You need to enter a username!");
-//            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
-        }
-        else if(email.isEmpty() || email.contains("@")) {
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else if (userTest != null && username.equals(userTest.getUsername())) {
+            request.setAttribute("usernameExists", "This username is already in use, please try another.");
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else if(!email.contains("@")) {
             request.setAttribute("emailError", "You need to enter a valid email address!");
-//           request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else if (emailTest != null && email.equals(emailTest.getEmail())) {
+            request.setAttribute("emailExists", "This email is already in use, please try another.");
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
         }
         else if (!password.equals(passwordConfirmation) || password.isEmpty()) {
-            request.setAttribute("passwordError", "The password is incorrect!");
-//            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+            request.setAttribute("passwordError", "The passwords do not match!");
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        } else {
+            correctInfo = true;
         }
 
-//       else if (password.isEmpty()){
-//            request.setAttribute("passwordEmptyError", "You need to enter a password!");
-////            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
 
-
-        request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+//        request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
 
 
 //        if (inputHasErrors) {
@@ -115,8 +64,10 @@ public class RegisterServlet extends HttpServlet {
             return;
         }
         // create and save a new user
-        User user = new User(username, email, password);
-        DaoFactory.getUsersDao().insert(user);
-        response.sendRedirect("/login");
+        if(correctInfo) {
+            User user = new User(username, email, password);
+            DaoFactory.getUsersDao().insert(user);
+            response.sendRedirect("/login");
+        }
     }
 }
